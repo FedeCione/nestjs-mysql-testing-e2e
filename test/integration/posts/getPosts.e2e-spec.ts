0ecd4e9e1d5f57717433a73d9f33774df8d2a24f
FedeCione <fede_cione_96@hotmail.com>
2024-07-04 16:35:58 -0300

    Refactor and fix

--- test/integration/posts/getPosts.e2e-spec.ts
+++ test/integration/posts/getPosts.e2e-spec.ts
@@ -5,77 +5,75 @@ import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../../../src/app.module';
 import { DataSource } from 'typeorm';
 
-export function postsGetTest() {
-  describe('/GET Posts', () => {
-    let app: INestApplication;
+describe('/GET Posts', () => {
+  let app: INestApplication;
 
-    // Initialize app before all tests
-    beforeAll(async () => {
-      const moduleFixture: TestingModule = await Test.createTestingModule({
-        imports: [AppModule],
-      }).compile();
+  // Initialize app before all tests
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
 
-      // Clean tables of database before all tests
-      app = moduleFixture.createNestApplication();
-      app.setGlobalPrefix('api');
-      await app.init();
+    // Clean tables of database before all tests
+    app = moduleFixture.createNestApplication();
+    app.setGlobalPrefix('api');
+    await app.init();
 
-      const dataSource = app.get(DataSource);
-      await dataSource.query('DELETE FROM posts');
-      await dataSource.query('DELETE FROM users');
-    });
+    const dataSource = app.get(DataSource);
+    await dataSource.query('DELETE FROM posts');
+    await dataSource.query('DELETE FROM users');
+  });
 
-    // Clean tables of database after each test
-    afterEach(async () => {
-      const dataSource = app.get(DataSource);
-      await dataSource.query('DELETE FROM posts');
-      await dataSource.query('DELETE FROM users');
-    });
+  // Clean tables of database after each test
+  afterEach(async () => {
+    const dataSource = app.get(DataSource);
+    await dataSource.query('DELETE FROM posts');
+    await dataSource.query('DELETE FROM users');
+  });
 
-    // Close app after all tests
-    afterAll(async () => {
-      await app.close();
-    });
+  // Close app after all tests
+  afterAll(async () => {
+    await app.close();
+  });
 
-    it('Return posts: 200 status code', async () => {
-      const createdUser = await request(app.getHttpServer())
-        .post('/api/users')
-        .send({
-          email: 'fede@gmail.com',
-          password: '123456',
-        });
-      const userId = createdUser.body.id;
+  it('Return posts: 200 status code', async () => {
+    const createdUser = await request(app.getHttpServer())
+      .post('/api/users')
+      .send({
+        email: 'fede@gmail.com',
+        password: '123456',
+      });
+    const userId = createdUser.body.id;
 
-      const createdPost = await request(app.getHttpServer())
-        .post('/api/posts')
-        .send({
+    const createdPost = await request(app.getHttpServer())
+      .post('/api/posts')
+      .send({
+        title: 'Post',
+        content: 'Post Content',
+        authorId: userId,
+      });
+    const postId = createdPost.body.id;
+
+    request(app.getHttpServer())
+      .get('/api/posts')
+      .expect([
+        {
+          id: postId,
           title: 'Post',
           content: 'Post Content',
           authorId: userId,
-        });
-      const postId = createdPost.body.id;
-
-      request(app.getHttpServer())
-        .get('/api/posts')
-        .expect([
-          {
-            id: postId,
-            title: 'Post',
-            content: 'Post Content',
-            authorId: userId,
-          },
-        ])
-        .expect(200);
-    });
-
-    it('Return posts: 404 status code', async () => {
-      request(app.getHttpServer())
-        .get('/api/posts')
-        .expect({
-          statusCode: 404,
-          message: 'NOT_FOUND - Post/s not found',
-        })
-        .expect(404);
-    });
+        },
+      ])
+      .expect(200);
   });
-}
+
+  it('Return posts: 404 status code', async () => {
+    request(app.getHttpServer())
+      .get('/api/posts')
+      .expect({
+        statusCode: 404,
+        message: 'NOT_FOUND - Post/s not found',
+      })
+      .expect(404);
+  });
+});
--- test/integration/users/getUsers.e2e-spec.ts
+++ test/integration/users/getUsers.e2e-spec.ts
@@ -4,71 +4,69 @@ import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../../../src/app.module';
 import { DataSource } from 'typeorm';
 
-export function usersGetTest() {
-  describe('/GET Users', () => {
-    let app: INestApplication;
+describe('/GET Users', () => {
+  let app: INestApplication;
 
-    // Initialize app before all tests
-    beforeAll(async () => {
-      const moduleFixture: TestingModule = await Test.createTestingModule({
-        imports: [AppModule],
-      }).compile();
+  // Initialize app before all tests
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
 
-      // Clean tables of database before all tests
-      app = moduleFixture.createNestApplication();
-      app.setGlobalPrefix('api');
-      await app.init();
+    // Clean tables of database before all tests
+    app = moduleFixture.createNestApplication();
+    app.setGlobalPrefix('api');
+    await app.init();
 
-      const dataSource = app.get(DataSource);
-      await dataSource.query('DELETE FROM posts');
-      await dataSource.query('DELETE FROM users');
-    });
+    const dataSource = app.get(DataSource);
+    await dataSource.query('DELETE FROM posts');
+    await dataSource.query('DELETE FROM users');
+  });
 
-    // Clean tables of database after each test
-    afterEach(async () => {
-      const dataSource = await app.get(DataSource);
-      await dataSource.query('DELETE FROM posts');
-      await dataSource.query('DELETE FROM users');
-    });
+  // Clean tables of database after each test
+  afterEach(async () => {
+    const dataSource = await app.get(DataSource);
+    await dataSource.query('DELETE FROM posts');
+    await dataSource.query('DELETE FROM users');
+  });
 
-    // Close app after all tests and clean table in database
-    afterAll(async () => {
-      await app.close();
-    });
+  // Close app after all tests and clean table in database
+  afterAll(async () => {
+    await app.close();
+  });
 
-    it('Get users: 200 status code', async () => {
-      const createdUser = await request(app.getHttpServer())
-        .post('/api/users')
-        .send({
-          email: 'fede@gmail.com',
-          password: '123456',
-        });
-      const userId = createdUser.body.id;
-      const createdAt = createdUser.body.createdAt;
-
-      return request(app.getHttpServer())
-        .get('/api/users')
-        .expect((res) => {
-          expect(res.body).toEqual(
-            expect.arrayContaining([
-              expect.objectContaining({
-                id: userId,
-                email: 'fede@gmail.com',
-                password: '123456',
-                createdAt: createdAt,
-                posts: [],
-              }),
-            ]),
-          );
-        })
-        .expect(200);
-    });
-
-    it('Get users: 404 status code', async () => {
-      return request(app.getHttpServer()).get('/api/users').expect({
-        statusCode: 404,
-        message: 'NOT_FOUND - User/s not found',
+  it('Get users: 200 status code', async () => {
+    const createdUser = await request(app.getHttpServer())
+      .post('/api/users')
+      .send({
+        email: 'fede@gmail.com',
+        password: '123456',
       });
+    const userId = createdUser.body.id;
+    const createdAt = createdUser.body.createdAt;
+
+    return request(app.getHttpServer())
+      .get('/api/users')
+      .expect((res) => {
+        expect(res.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              id: userId,
+              email: 'fede@gmail.com',
+              password: '123456',
+              createdAt: createdAt,
+              posts: [],
+            }),
+          ]),
+        );
+      })
+      .expect(200);
+  });
+
+  it('Get users: 404 status code', async () => {
+    return request(app.getHttpServer()).get('/api/users').expect({
+      statusCode: 404,
+      message: 'NOT_FOUND - User/s not found',
     });
   });
-}
+});
